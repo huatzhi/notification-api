ea5d03141e2f1a55671759f65cff99ad1d9af0a3
Huat Zhi <huatzhi@gmail.com>
2019-09-26 19:38:46 +0800

    simple boilerplate

---
+++ app.js
@@ -0,0 +1,12 @@
+const Express = require('express');
+const app = Express();
+// todo :: take it from .env
+const port = 9000; 
+
+
+
+app.use(function (req, res, next) {
+  res.status(404).json({message: "Path not exist."});
+})
+
+app.listen(port, () => console.log(`App start on port ${port}!`));
