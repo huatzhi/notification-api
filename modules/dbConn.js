9204a09703c9c08948846e685d34b0927145c763
Huat Zhi <huatzhi@gmail.com>
2019-09-29 22:22:07 +0800

    change the default dbpass to suit default password

--- modules/dbConn.js
+++ modules/dbConn.js
@@ -6,7 +6,7 @@ import Sequelize from 'sequelize';
 export const dbConn = new Sequelize(
   process.env.DB_NAME || "notification_api", // database name 
   process.env.DB_USER || "root", // database user
-  process.env.DB_PASS || "abc123", // database pw
+  process.env.DB_PASS || "root", // database pw
   {
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || "3306",
